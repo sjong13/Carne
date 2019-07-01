43eb2ac55b73a405a69c076fdb6bdb88da2be0d5
Gnoj Nehpets <sjong13@gmail.com>
2019-07-01 13:55:19 -0500

    fontawesome icons working; added account button in restaurant view

--- Carne/Carne/Pages/OwnerPageDetail.xaml.cs
+++ Carne/Carne/Pages/OwnerPageDetail.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Carne.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using static Carne.Pages.OwnerPageMaster;
@@ -13,10 +13,24 @@ namespace Carne.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class OwnerPageDetail : ContentPage
     {
+        public DailyRecommendationsPageViewModel ViewModel;
         public OwnerPageDetail()
         {
             BindingContext = new OwnerPageMasterViewModel();
+            ViewModel = new DailyRecommendationsPageViewModel();
             InitializeComponent();
+        }
+
+        private async void AccessAccountPopup(object sender, EventArgs e)
+        {
+            var result = await DisplayActionSheet(ViewModel.TestUser.UserName + "'s" + " Account", "Cancel", null, "Logout");
+
+            switch (result)
+            {
+                case "Logout":
+                    await Navigation.PushModalAsync(new LoginPage());
+                    break;
+            }
         }
     }
 }
